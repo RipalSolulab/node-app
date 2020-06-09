e9502a700632620902e4fb67bc881aa5e4622b2c
shahripal400 <shahripal400@gmail.com>
2020-06-10 00:08:10 +0530

    simple node app with test

---
+++ test/test.js
@@ -0,0 +1,11 @@
+var request = require('supertest');
+var app = require('../index.js');
+
+describe('GET /', function(){
+  it('respond with hello world',function(done){
+
+//navigate to root and check the 
+ request(app).get('/').expect('hello world', done);
+ });
+});
+
